f0f1ef8405a3475ccb5c13a64cb28cc622b4020f
javierrome236 <37107269+javierrome236@users.noreply.github.com>
2025-07-28 13:41:47 +0200

    Update server.py

--- server.py
+++ server.py
@@ -21,7 +21,8 @@ bcrypt = Bcrypt()
 app.config["MYSQL_DATABASE_HOST"]	= "localhost"
 app.config["MYSQL_DATABASE_PORT"]	= 3306
 app.config["MYSQL_DATABASE_USER"]	= "root"
-app.config["MYSQL_DATABASE_PASSWORD"]	= "root"
+app.config["MYSQL_DATABASE_PASSWORD"]	= "mipassword"
+#app.config["MYSQL_DATABASE_PASSWORD"]	= "root"
 app.config["MYSQL_DATABASE_DB"] = "cuidatiacogdb"
 
 mysql.init_app(app)
